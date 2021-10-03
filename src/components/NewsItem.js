408eb48318efcb8a7eba1de4cda6f57c787b8a9c
dimension <dimension2414@gmail.com>
2021-10-03 21:01:04 +0530

    working with states and json file

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
+import { getByTitle } from "@testing-library/dom";
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
-    render() {
-        return (
-            <div>
-                <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="/" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-            </div>
-        )
-    }
+  render() {
+    let { title, description, imageUrl, newsUrl} = this.props;
+    return (
+      <div>
+        <div class="card" style={{width: "18rem"}}>
+          <img src={imageUrl} class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{description}</p>
+            <a href="/" class="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
