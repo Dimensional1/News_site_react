ebf7a3c4dfdd7a877d7b426783b5d29abd62eadd
dimension <dimension2414@gmail.com>
2021-10-08 17:53:55 +0530

    adding prev and next button

--- src/components/News.js
+++ src/components/News.js
@@ -2,40 +2,89 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
- 
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
-    }
+      page: 1,
+    };
   }
 
   async componentDidMount() {
-    let url="https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=f2ddb23f8d2b418caafa0492e361ce5e";
-    let data =await fetch(url);
-    let parsedData=await data.json()
-    this.setState({articles: parsedData.articles})
+    let url =
+      "https://newsapi.org/v2/everything?q=apple&from=2021-10-05&to=2021-10-05&sortBy=popularity&apiKey=f2ddb23f8d2b418caafa0492e361ce5e&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+    });
   }
-  
+
+  handledPreviousClick = async () => {
+    console.log("clicked previous button");
+    let url = `https://newsapi.org/v2/everything?q=apple&from=2021-10-05&to=2021-10-05&sortBy=popularity&apiKey=f2ddb23f8d2b418caafa0492e361ce5e&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handledNextClick = async () => {
+    if (Math.ceil(this.state.page + 1 > this.state.totalArticles / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/everything?q=apple&from=2021-10-05&to=2021-10-05&sortBy=popularity&apiKey=f2ddb23f8d2b418caafa0492e361ce5e&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     return (
-      <div className="container my-6">
-        <h1 className="mb-6">News-Monkey Top Headlines</h1>
-        <div className="row">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-4" key={element.url}  >
-              <NewsItem
-                title={element.title?element.title.slice(0,40):""}
-                description={element.description?element.description:""}
-                imageUrl={element.urlToImage}
-                newsUrl={element.url}
-              />
-            </div>;
+      <div className="container my-3 center">
+        <h1 className="mb-6 my-3">News-Monkey Top Headlines</h1>
+        <div className="row d-flex justify-content-center">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4 mx-5" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 40) : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
         </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            className="btn btn-dark"
+            onClick={this.handledPreviousClick}
+          >
+            🦘Previous
+          </button>
+          <button className="btn btn-success" onClick={this.handledNextClick}>
+            Next 🐱‍🏍
+          </button>
+        </div>
       </div>
-    )
+    );
   }
 }
-
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,17 @@
-import { getByTitle } from "@testing-library/dom";
+
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
     let { title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        <div className="card" style={{width: "18rem"}}>
-          <img src={!imageUrl?"https://www.google.com/search?q=shin+chan&sxsrf=AOaemvIgkv8y_ZU7nHF-3xOa9GS0UuLyow:1633453194902&source=lnms&tbm=isch&sa=X&ved=2ahUKEwirs8Gr37PzAhXEumMGHfP_BmgQ_AUoAXoECAEQAw&biw=1280&bih=623&dpr=1.5#imgrc=EcSssSxje2XrcM":imageUrl} className="card-img-top" alt="..." />
+      <div className="container"> 
+        <div className="card my-3 " style={{width: "18rem"}}>
+          <img src={!imageUrl?"https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F6%2F6b%2FTaka_Shiba.jpg%2F1200px-Taka_Shiba.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FShiba_Inu&tbnid=UvBK8t-8cyW1mM&vet=12ahUKEwi-mrKstLXzAhWyoksFHbVVD_cQMygBegUIARC5AQ..i&docid=OwGJIKvbjVKVqM&w=1200&h=900&q=shiba%20images&ved=2ahUKEwi-mrKstLXzAhWyoksFHbVVD_cQMygBegUIARC5AQ":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target=" _blank" className="btn btn-sm btn-primary">
+            <a href={newsUrl} target=" _blank" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
